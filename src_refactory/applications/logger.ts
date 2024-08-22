0968f3f57b805713ed523125eb97bf129c8add82
cesxhin <mattia.ceschin@gmail.com>
2024-08-22 21:11:02 +0200

    Update gitignore and add src_refactory

---
+++ src_refactory/applications/logger.ts
@@ -0,0 +1,56 @@
+import chalk from "chalk";
+import {DateTime} from "luxon";
+
+class Logger {
+    nameService: string | null = null;
+
+    constructor(nameService: string){
+        this.nameService = nameService.toUpperCase();
+    }
+
+    debug(...args: Array<any>){
+        this.#baseLog("debug", ...args);
+    }
+
+    info(...args: Array<any>){
+        this.#baseLog("info", ...args);
+    }
+
+    warn(...args: Array<any>){
+        this.#baseLog("warning", ...args);
+    }
+
+    error(...args: Array<any>){
+        this.#baseLog("error", ...args);
+    }
+
+    fatal(...args: Array<any>){
+        this.#baseLog("fatal", ...args);
+    }
+
+    #baseLog(type: "debug" | "info" | "warning" | "error" | "fatal", ...args: Array<any>){
+        let message = `[${DateTime.now().toFormat("dd-MM-yyyy hh:mm:ssZZ")}] [${type.toUpperCase()}] [${this.nameService}]`
+        
+        switch(type){
+            case "debug":
+                message = chalk.gray(message, ...args);
+                break;
+            case "info":
+                message = chalk.blue(message, ...args);
+                break;
+            case "warning":
+                message = chalk.yellow(message, ...args);
+                break;
+            case "error":
+                message = chalk.red(message, ...args);
+                break;
+            case "fatal":
+                message = chalk.redBright(message, ...args);
+                break;
+        }
+
+        console.log(message);
+    }
+}
+
+export default Logger;
